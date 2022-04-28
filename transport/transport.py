0ec52d0f65b329d7f4634182741657fa86f3e674
Joram Buitenhuis <jej.buitenhuis@student.han.nl>
2022-04-28 11:23:32 +0200

    Move transport script to seperate docker container

--- transport/transport.py
+++ transport/transport.py
@@ -3,11 +3,14 @@ Some beautiful python code to transfer data from MSSQL to Mongo
 """
 
 import pyodbc
+import time
 
-CONNECTION_URL_MSSQL = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost,14330;UID=sa;PWD=Football!;DATABASE=flm"
+CONNECTION_URL_MSSQL = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=db_mssql,1433;UID=sa;PWD=Football!;DATABASE=flm"
 CONNECTION_URL_MONGO = ""
 
 def main():
+	time.sleep(15)
+
 	connection = pyodbc.connect(CONNECTION_URL_MSSQL)
 
 	cursor = connection.cursor()
