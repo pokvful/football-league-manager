d1ac4696c9821c84f9041a97cb4659e1a588e952
Joram Buitenhuis <jej.buitenhuis@student.han.nl>
2022-04-28 10:12:05 +0200

    Switch to ODBC

--- transport/transport.py
+++ transport/transport.py
@@ -2,35 +2,21 @@
 Some beautiful python code to transfer data from MSSQL to Mongo
 """
 
-import pymssql
+import pyodbc
 
-CONNECTION_URL_MSSQL = {
-	"server": "localhost:14330",
-	"user": "sa",
-	"password": "Football!",
-	"database": "flm",
-}
-CONNECTION_URL_MONGO = {
-	"server": "",
-	"user": "",
-	"password": "",
-	"database": "",
-}
+CONNECTION_URL_MSSQL = "DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost,14330;UID=sa;PWD=Football!;DATABASE=flm"
+CONNECTION_URL_MONGO = ""
 
 def main():
-	connection = pymssql.connect(
-		CONNECTION_URL_MSSQL["server"],
-		CONNECTION_URL_MSSQL["user"],
-		CONNECTION_URL_MSSQL["password"],
-		CONNECTION_URL_MSSQL["database"],
-	)
+	connection = pyodbc.connect(CONNECTION_URL_MSSQL)
 
-	cursor = connection.cursor(as_dict=True)
+	cursor = connection.cursor()
 
 	cursor.execute("SELECT * FROM MOCK_DATA")
+	rows = cursor.fetchall()
 
-	for row in cursor:
-		print( "ID=%d, first_name=%s" % ( row["id"], row["first_name"] ) )
+	for row in rows:
+		print( "ID=%d, first_name=%s" % ( row.id, row.first_name ) )
 
 	connection.close()
 
