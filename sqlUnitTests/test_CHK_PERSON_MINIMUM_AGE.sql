b74fd5af542658d09d4592d422e5fb701549d662
ThomKraaijvanger <t.kraaijvanger1@student.han.nl>
2022-05-11 14:48:43 +0200

    Now using proper calculation + added 2 edge cases

--- sqlUnitTests/test_CHK_PERSON_MINIMUM_AGE.sql
+++ sqlUnitTests/test_CHK_PERSON_MINIMUM_AGE.sql
@@ -11,11 +11,11 @@ BEGIN
     EXEC TSQLT.ExpectException
 
 	INSERT INTO PERSON (PERSON_ID, COUNTRY_NAME, FIRST_NAME, LAST_NAME, MIDDLE_NAME, BIRTH_DATE)
-    VALUES(1, 'Netherlands', 'Pieter', ' Post', null, GETDATE() )
+    VALUES(1, 'Netherlands', 'Pieter', ' Post', null, GETDATE())
 END;
 GO
 
-EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING]'
+EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING]'
 GO
 
 CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING]
@@ -35,3 +35,40 @@ GO
 
 EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING]'
 GO
+
+CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING EDGE]
+AS
+BEGIN
+    EXEC TSQLT.FAKETABLE 'dbo.PERSON'
+
+    EXEC TSQLT.APPLYCONSTRAINT
+        @TableName = 'dbo.PERSON', @ConstraintName = 'CHK_PERSON_HAS_VALID_AGE'
+
+    EXEC TSQLT.ExpectNoException
+
+	INSERT INTO PERSON (PERSON_ID, COUNTRY_NAME, FIRST_NAME, LAST_NAME, MIDDLE_NAME, BIRTH_DATE)
+    VALUES(1, 'Netherlands', 'Pieter', ' Post', null, '2007-01-01' )
+END;
+GO
+
+EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING EDGE]'
+GO
+
+CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING EDGE]
+AS
+BEGIN
+    EXEC TSQLT.FAKETABLE 'dbo.PERSON'
+
+    EXEC TSQLT.APPLYCONSTRAINT
+        @TableName = 'dbo.PERSON', @ConstraintName = 'CHK_PERSON_HAS_VALID_AGE'
+
+    EXEC TSQLT.ExpectException
+
+	INSERT INTO PERSON (PERSON_ID, COUNTRY_NAME, FIRST_NAME, LAST_NAME, MIDDLE_NAME, BIRTH_DATE)
+    VALUES(1, 'Netherlands', 'Pieter', ' Post', null, '2007-08-08' )
+END;
+GO
+
+EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING EDGE]'
+GO
+
