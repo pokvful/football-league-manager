a946a9c25d60660e83ad03f6437df6a6f6290320
Junjie Juan <jj.juan@student.han.nl>
2022-05-11 13:17:37 +0200

    reformat the code for code convention

--- sqlUnitTests/test_TRG_MINIMUM_PLAYERS_IN_CLUB.sql
+++ sqlUnitTests/test_TRG_MINIMUM_PLAYERS_IN_CLUB.sql
@@ -12,14 +12,14 @@ EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.PLAYER', @triggername = 'TRG_MINIM
 --Act
 EXEC [tSQLt].[ExpectException] 'There must be at least 11 players in a club'
 INSERT INTO dbo.PLAYER(PERSON_ID, CLUB_NAME,JERSEY)
-VALUES  (1,'TESTCLUB1', 1),
-        (2,'TESTCLUB1', 2),
-        (3,'TESTCLUB1', 3),
-        (4,'TESTCLUB1', 4),
-        (5,'TESTCLUB1', 5),
-        (6,'TESTCLUB1', 6),
-        (7,'TESTCLUB1', 7),
-        (8,'TESTCLUB1', 8)
+VALUES	(1,'TESTCLUB1', 1),
+		(2,'TESTCLUB1', 2),
+		(3,'TESTCLUB1', 3),
+		(4,'TESTCLUB1', 4),
+		(5,'TESTCLUB1', 5),
+		(6,'TESTCLUB1', 6),
+		(7,'TESTCLUB1', 7),
+		(8,'TESTCLUB1', 8)
 --Assert
 
 end
@@ -37,17 +37,17 @@ EXEC [tSQLt].[ApplyTrigger] @tablename = 'dbo.PLAYER', @triggername = 'TRG_MINIM
 EXEC [tSQLt].[ExpectNoException]
 INSERT INTO dbo.PLAYER(PERSON_ID, CLUB_NAME,JERSEY)
 VALUES  (1,'TESTCLUB1', 1),
-        (2,'TESTCLUB1', 2),
-        (3,'TESTCLUB1', 3),
-        (4,'TESTCLUB1', 4),
-        (5,'TESTCLUB1', 5),
-        (6,'TESTCLUB1', 6),
-        (7,'TESTCLUB1', 7),
-        (8,'TESTCLUB1', 8),
-        (9,'TESTCLUB1', 9),
-        (10,'TESTCLUB1', 10),
-        (11,'TESTCLUB1', 11),
-        (12,'TESTCLUB1', 12)
+		(2,'TESTCLUB1', 2),
+		(3,'TESTCLUB1', 3),
+		(4,'TESTCLUB1', 4),
+		(5,'TESTCLUB1', 5),
+		(6,'TESTCLUB1', 6),
+		(7,'TESTCLUB1', 7),
+		(8,'TESTCLUB1', 8),
+		(9,'TESTCLUB1', 9),
+		(10,'TESTCLUB1', 10),
+		(11,'TESTCLUB1', 11),
+		(12,'TESTCLUB1', 12)
 --Assert
 
 end
