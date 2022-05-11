7229f62228c796e2fe95f1bd58425b4800d464eb
Joram Buitenhuis <jej.buitenhuis@student.han.nl>
2022-05-11 13:23:44 +0200

    Add CLUB mock data

---
+++ dockerFiles/mssql/12-insert_club.sql
@@ -0,0 +1,10 @@
+set nocount on;
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Wikido', 'Visine Tears', 'Sweden', 'Åmål', 5);
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Voonte', 'Triple Vitamin Drops with Fluoride', 'Tanzania', 'Kisesa', 9);
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Topicblab', 'White Alder', 'Togo', 'Badou', 2);
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Topdrive', 'Wasp hymenoptera venom Venomil Maintenance', 'Tunisia', 'Bizerte', 3);
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Tavu', 'Vanilla Cupcake Antibacterial Hand Sanitizer', 'United States', 'Detroit', 8);
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Quimm', 'Velivet', 'Vietnam', 'K Bang', 6);
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Feedmix', 'Warfarin Sodium', 'Zimbabwe', 'Chipinge', 4);
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Fadeo', 'YES TO TOMATOES ACNE CONTROL GEL CLEANSER', 'Zimbabwe', 'Odzi', 1);
+insert into CLUB (Club_name, Stadium_name, Country_name, City_name, Coach_person_id) values ('Eadel', 'Vaporizing Chest Rub', 'Portugal', 'São Manços', 7);
