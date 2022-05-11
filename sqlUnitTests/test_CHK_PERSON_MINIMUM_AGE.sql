88c851afd50e36b4d54d2d6ec71e5b34e1ef69ad
ThomKraaijvanger <t.kraaijvanger1@student.han.nl>
2022-05-11 13:43:01 +0200

    Added unit tests

---
+++ sqlUnitTests/test_CHK_PERSON_MINIMUM_AGE.sql
@@ -0,0 +1,37 @@
+EXEC tSQLt.NewTestClass 'TEST_CHK_PERSON_MINIMUM_AGE'
+GO
+CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING]
+AS
+BEGIN
+    EXEC TSQLT.FAKETABLE 'dbo.PERSON'
+
+    EXEC TSQLT.APPLYCONSTRAINT
+        @TableName = 'dbo.PERSON', @ConstraintName = 'CHK_PERSON_HAS_VALID_AGE'
+
+    EXEC TSQLT.ExpectException
+
+	INSERT INTO PERSON (PERSON_ID, COUNTRY_NAME, FIRST_NAME, LAST_NAME, MIDDLE_NAME, BIRTH_DATE)
+    VALUES(1, 'Netherlands', 'Pieter', ' Post', null, GETDATE() )
+END;
+GO
+
+EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING]'
+GO
+
+CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING]
+AS
+BEGIN
+    EXEC TSQLT.FAKETABLE 'dbo.PERSON'
+
+    EXEC TSQLT.APPLYCONSTRAINT
+        @TableName = 'dbo.PERSON', @ConstraintName = 'CHK_PERSON_HAS_VALID_AGE'
+
+    EXEC TSQLT.ExpectNoException
+
+	INSERT INTO PERSON (PERSON_ID, COUNTRY_NAME, FIRST_NAME, LAST_NAME, MIDDLE_NAME, BIRTH_DATE)
+    VALUES(1, 'Netherlands', 'Pieter', ' Post', null, '1980-01-01' )
+END;
+GO
+
+EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING]'
+GO
