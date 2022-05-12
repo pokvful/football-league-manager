c58f6cecb8a1469eae1f16392d94f8dcca4af40e
ThomKraaijvanger <t.kraaijvanger1@student.han.nl>
2022-05-12 10:49:03 +0200

    forgot latest add in last commit

--- sqlUnitTests/test_CHK_PERSON_MINIMUM_AGE.sql
+++ sqlUnitTests/test_CHK_PERSON_MINIMUM_AGE.sql
@@ -1,74 +1,59 @@
 EXEC tSQLt.NewTestClass 'TEST_CHK_PERSON_MINIMUM_AGE'
 GO
-CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING]
+CREATE OR ALTER PROCEDURE TEST_CHK_PERSON_MINIMUM_AGE.SETUP
 AS
 BEGIN
     EXEC TSQLT.FAKETABLE 'dbo.PERSON'
 
     EXEC TSQLT.APPLYCONSTRAINT
         @TableName = 'dbo.PERSON', @ConstraintName = 'CHK_PERSON_HAS_VALID_AGE'
-
-    EXEC TSQLT.ExpectException
-
-	INSERT INTO PERSON (PERSON_ID, COUNTRY_NAME, FIRST_NAME, LAST_NAME, MIDDLE_NAME, BIRTH_DATE)
-    VALUES(1, 'Netherlands', 'Pieter', ' Post', null, GETDATE())
-END;
+END
 GO
 
+CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING]
+AS
+BEGIN
+    EXEC TSQLT.ExpectException
+
+	INSERT INTO PERSON (BIRTH_DATE)
+    VALUES(GETDATE())
+END;
+GO
 EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING]'
 GO
 
 CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING]
 AS
 BEGIN
-    EXEC TSQLT.FAKETABLE 'dbo.PERSON'
-
-    EXEC TSQLT.APPLYCONSTRAINT
-        @TableName = 'dbo.PERSON', @ConstraintName = 'CHK_PERSON_HAS_VALID_AGE'
-
     EXEC TSQLT.ExpectNoException
 
-	INSERT INTO PERSON (PERSON_ID, COUNTRY_NAME, FIRST_NAME, LAST_NAME, MIDDLE_NAME, BIRTH_DATE)
-    VALUES(1, 'Netherlands', 'Pieter', ' Post', null, '1980-01-01' )
+	INSERT INTO PERSON (BIRTH_DATE)
+    VALUES('1980-01-01' )
 END;
 GO
-
 EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING]'
 GO
 
 CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING EDGE]
 AS
 BEGIN
-    EXEC TSQLT.FAKETABLE 'dbo.PERSON'
-
-    EXEC TSQLT.APPLYCONSTRAINT
-        @TableName = 'dbo.PERSON', @ConstraintName = 'CHK_PERSON_HAS_VALID_AGE'
-
     EXEC TSQLT.ExpectNoException
 
 	INSERT INTO PERSON (BIRTH_DATE)
     VALUES(DATEADD(year, -15, GETDATE()))
 END;
 GO
-
 EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old PASSING EDGE]'
 GO
 
 CREATE OR ALTER PROCEDURE [TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING EDGE]
 AS
 BEGIN
-    EXEC TSQLT.FAKETABLE 'dbo.PERSON'
-
-    EXEC TSQLT.APPLYCONSTRAINT
-        @TableName = 'dbo.PERSON', @ConstraintName = 'CHK_PERSON_HAS_VALID_AGE'
-
     EXEC TSQLT.ExpectException
 
 	INSERT INTO PERSON (BIRTH_DATE)
     VALUES(DATEADD(day, 1, DATEADD(year, -15, GETDATE())))
 END;
 GO
-
 EXEC tSQLt.Run '[TEST_CHK_PERSON_MINIMUM_AGE].[Test that checks if the person is at least 15 years old FAILING EDGE]'
 GO
-
