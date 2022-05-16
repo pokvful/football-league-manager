293f2cf29cae1a17985c9049520639bb93837d22
Tim Meuwsen <tim.meuwsen@student.han.nl>
2022-05-16 15:23:45 +0200

    removed duplicate code for data-analist user

--- dockerFiles/mongo/3-create-user-data-analist.js
+++ dockerFiles/mongo/3-create-user-data-analist.js
@@ -4,12 +4,4 @@ db.createUser(
     pwd:  "Football@",
     roles: [ { role: "read", db: "flm" } ]
   }
-),
-
-db.createUser(
-  {
-    user: "transport",
-    pwd:  "Tr@nsport123!",
-    roles: [ { role: "create, drop", db: "flm" } ]
-  }
 )
