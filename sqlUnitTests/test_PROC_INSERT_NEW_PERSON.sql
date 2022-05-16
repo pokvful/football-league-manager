13df46e7f163c9e1bd92ceb4363430d2e76e89ff
Tim Meuwsen <tim.meuwsen@student.han.nl>
2022-05-16 13:16:21 +0200

    finished unit tests and fixed output method in procedures

--- sqlUnitTests/test_PROC_INSERT_NEW_PERSON.sql
+++ sqlUnitTests/test_PROC_INSERT_NEW_PERSON.sql
@@ -11,83 +11,71 @@ BEGIN
 	EXEC tSQLt.FakeTable @tableName = 'dbo.Referee';
 	EXEC tSQLt.FakeTable @tableName = 'dbo.Coach';
 
-	INSERT INTO dbo.Player (PERSON_ID, CLUB_NAME, JERSEY)
-	VALUES (1, 'Club', 1);
-
 END
 
 GO
 
-CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_PLAYER_SUCCESS]
+CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_PLAYER_VALID_DATA]
 AS
 BEGIN
-	EXEC TSQLT.ExpectNoException;
 
 	EXEC PROC_INSERT_NEW_PLAYER 'Country', 'FirstName', 'LastName', 'MiddleName', '2000-10-04', 'Club', 1
 
+	CREATE TABLE expected
+	(
+		PERSON_ID INT,
+		CLUB_NAME VARCHAR(128),
+		JERSEY    INT
+	);
+
+	INSERT INTO expected (PERSON_ID, CLUB_NAME, JERSEY)
+	VALUES (1, 'Club', 1)
+
+	EXEC tSQLt.AssertEqualsTable @Expected='expected',
+		 @Actual='dbo.Player'
 END
 
 GO
 
-CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_PLAYER_FAILS]
+CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_REFEREE_VALID_DATA]
 AS
 BEGIN
-
-	EXEC TSQLT.ExpectException;
-
-	EXEC PROC_INSERT_NEW_PLAYER 'Country', 'FirstName', 'LastName', 'MiddleName', '2000-10-04', 'Club', 1
-
-END
-
-GO
-
-CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_REFEREE_SUCCESS]
-AS
-BEGIN
-
-	EXEC TSQLT.ExpectNoException;
 
 	EXEC PROC_INSERT_NEW_REFEREE @Country_name = 'Country', @First_name = 'Piet', @Last_name = 'Jan',
 		 @Middle_name = 'van', @Birth_Date = '2000-10-04'
 
+	CREATE TABLE expected
+	(
+		PERSON_ID INT
+	);
+
+	INSERT INTO expected (PERSON_ID)
+	VALUES (1)
+
+	EXEC tSQLt.AssertEqualsTable @Expected='expected',
+		 @Actual='dbo.Referee'
 END
 
 GO
 
-CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_REFEREE_FAILS]
+CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_COACH_VALID_DATA]
 AS
 BEGIN
-
-	EXEC TSQLT.ExpectException;
-
-	EXEC PROC_INSERT_NEW_REFEREE @Country_name = 'Country', @First_name = 'Piet', @Last_name = 'Jan',
-		 @Middle_name = 'van', @Birth_Date = '2000-10-04'
-
-END
-
-GO
-
-CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_COACH_SUCCESS]
-AS
-BEGIN
-
-	EXEC TSQLT.ExpectNoException;
 
 	EXEC PROC_INSERT_NEW_COACH @Country_name = 'Country', @First_name = 'Piet', @Last_name = 'Jan',
 		 @Middle_name = 'van', @Birth_Date = '2000-10-04'
 
-END
+	CREATE TABLE expected
+	(
+		PERSON_ID INT
+	);
 
-GO
+	INSERT INTO expected (PERSON_ID)
+	VALUES (1)
 
-CREATE OR ALTER PROCEDURE test_PROC_INSERT_NEW_PERSON.[test_PROC_INSERT_NEW_COACH_FAILS]
-AS
-BEGIN
+	EXEC tSQLt.AssertEqualsTable @Expected='expected',
+		 @Actual='dbo.Coach'
 
-	EXEC TSQLT.ExpectException;
-
-	EXEC PROC_INSERT_NEW_COACH @Country_name = 'Country', @First_name = 'Piet', @Last_name = 'Jan',
-		 @Middle_name = 'van', @Birth_Date = '2000-10-04'
 END
 
 GO
