d95fc8331090aff0c2366d62e0bee50f23cfb0ea
Oktay Soytürk <o.soyturk@student.han.nl>
2022-05-17 14:04:25 +0200

    Updated: naming

--- dockerFiles/mssql/901-TRG_PERSON_IS_PLAYER_OR_COACH.sql
+++ dockerFiles/mssql/901-TRG_PERSON_IS_PLAYER_OR_COACH.sql
@@ -1,4 +1,4 @@
-CREATE OR ALTER TRIGGER trg_person_is_player_or_coach_yellow
+CREATE OR ALTER TRIGGER TRG_PERSON_IS_PLAYER_OR_COACH_YELLOW
 	ON yellow_card
 	FOR INSERT, UPDATE
 	AS
@@ -9,9 +9,9 @@ BEGIN
 
 	BEGIN TRY
 		IF EXISTS(
-			SELECT inserted.person_id
+			SELECT inserted.PERSON_ID
 			FROM inserted
-					 JOIN referee ON inserted.person_id = referee.person_id
+					 JOIN referee ON inserted.PERSON_ID = referee.PERSON_ID
 			)
 			THROW 50001, 'Referee can not have a yellow card!', 1;
 
@@ -19,17 +19,17 @@ BEGIN
 			SELECT 1
 			FROM inserted, (SELECT player_person_id as person_id
 				  FROM player_played_in_match, inserted
-				  WHERE player_played_in_match.match_id = inserted.match_id
+				  WHERE player_played_in_match.MATCH_ID = inserted.MATCH_ID
 				  UNION
 				  SELECT coach_person_id
 				  FROM club
 				  WHERE club_name = (SELECT out_club_name
 									 FROM match, inserted
-									 WHERE match.match_id = inserted.match_id)
+									 WHERE match.MATCH_ID = inserted.MATCH_ID)
 					 OR club_name = (SELECT home_club_name
 									 FROM match, inserted
-									 WHERE match.match_id = inserted.match_id)) AS check_if_player_exists_in_match
-			WHERE check_if_player_exists_in_match.person_id = inserted.person_id
+									 WHERE match.MATCH_ID = inserted.MATCH_ID)) AS check_if_player_exists_in_match
+			WHERE check_if_player_exists_in_match.PERSON_ID = inserted.PERSON_ID
 			)
 			THROW 50001, 'Player/coach is not available in match!', 1;
 	END TRY
@@ -39,7 +39,7 @@ BEGIN
 END
 GO
 
-CREATE OR ALTER TRIGGER trg_person_is_player_or_coach_red
+CREATE OR ALTER TRIGGER TRG_PERSON_IS_PLAYER_OR_COACH_RED
 	ON red_card
 	FOR INSERT, UPDATE
 	AS
@@ -50,9 +50,9 @@ BEGIN
 
 	BEGIN TRY
 		IF EXISTS(
-			SELECT inserted.person_id
+			SELECT inserted.PERSON_ID
 			FROM inserted
-					 JOIN referee ON inserted.person_id = referee.person_id
+					 JOIN referee ON inserted.PERSON_ID = referee.PERSON_ID
 			)
 			THROW 50001, 'Referee can not have a red card!', 1;
 
@@ -60,17 +60,17 @@ BEGIN
 			SELECT 1
 			FROM inserted, (SELECT player_person_id as person_id
 				  FROM player_played_in_match, inserted
-				  WHERE player_played_in_match.match_id = inserted.match_id
+				  WHERE player_played_in_match.MATCH_ID = inserted.MATCH_ID
 				  UNION
 				  SELECT coach_person_id
 				  FROM club
 				  WHERE club_name = (SELECT out_club_name
 									 FROM match, inserted
-									 WHERE match.match_id = inserted.match_id)
+									 WHERE match.MATCH_ID = inserted.MATCH_ID)
 					 OR club_name = (SELECT home_club_name
 									 FROM match, inserted
-									 WHERE match.match_id = inserted.match_id)) AS check_if_player_exists_in_match
-			WHERE check_if_player_exists_in_match.person_id = inserted.person_id
+									 WHERE match.MATCH_ID = inserted.MATCH_ID)) AS check_if_player_exists_in_match
+			WHERE check_if_player_exists_in_match.PERSON_ID = inserted.PERSON_ID
 			)
 			THROW 50001, 'Player/coach is not available in match!', 1;
 	END TRY
