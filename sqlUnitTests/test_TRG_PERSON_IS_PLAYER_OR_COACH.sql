bd4e7e500bc4cec550a196edbf32fab073581457
Oktay Soytürk <o.soyturk@student.han.nl>
2022-05-17 14:50:07 +0200

    Added: setup for tests to remove duplicate code

--- sqlUnitTests/test_TRG_PERSON_IS_PLAYER_OR_COACH.sql
+++ sqlUnitTests/test_TRG_PERSON_IS_PLAYER_OR_COACH.sql
@@ -6,7 +6,7 @@
 
 EXEC tsqlt.newtestclass 'test_TRG_PERSON_IS_PLAYER_OR_COACH_YELLOW';
 
-CREATE OR ALTER PROCEDURE [Test_Trg_Person_Is_Player_Or_Coach_Yellow].[Test_Trg_Person_Is_Player_Or_Coach_Yellow_Player_Not_Available_Exception]
+CREATE OR ALTER PROCEDURE Test_Trg_Person_Is_Player_Or_Coach_Yellow.SETUP
 AS
 BEGIN
 	-- Assemble:
@@ -38,7 +38,12 @@ BEGIN
 	VALUES (3, 1, 'Attacker');
 
 	EXEC tsqlt.applytrigger 'YELLOW_CARD', 'TRG_PERSON_IS_PLAYER_OR_COACH_YELLOW';
+END
+GO
 
+CREATE OR ALTER PROCEDURE [Test_Trg_Person_Is_Player_Or_Coach_Yellow].[Test_Trg_Person_Is_Player_Or_Coach_Yellow_Player_Not_Available_Exception]
+AS
+BEGIN
 	-- Expect:
 	EXEC tsqlt.expectexception @expectedmessage = 'Player/coach is not available in match!';
 
@@ -61,36 +66,6 @@ GO
 CREATE OR ALTER PROCEDURE [Test_Trg_Person_Is_Player_Or_Coach_Yellow].[Test_Trg_Person_Is_Player_Or_Coach_Yellow_Player_Is_Referee_Exception]
 AS
 BEGIN
-	-- Assemble:
-	EXEC tsqlt.faketable 'PERSON';
-	EXEC tsqlt.faketable 'REFEREE';
-	EXEC tsqlt.faketable 'MATCH';
-	EXEC tsqlt.faketable 'PLAYER_AS_RESERVE_IN_MATCH';
-	EXEC tsqlt.faketable 'POSITION';
-	EXEC tsqlt.faketable 'YELLOW_CARD';
-
-	INSERT INTO person([Person_Id], [Country_Name], [First_Name], [Last_Name], [Middle_Name], [Birth_Date])
-	VALUES (1, 'Nederland', 'Oktay', 'Soyturk', NULL, '1999-05-24'),
-		   (2, 'Nederland', 'Yatko', 'Krutyos', NULL, '1999-05-31'),
-		   (3, 'Portugal', 'Cristiano', 'Ronaldo', NULL, '1999-05-11'),
-		   (4, 'Argentina', 'Lionel', 'Messi', NULL, '1999-02-15')
-
-	INSERT INTO referee([Person_Id])
-	VALUES (1);
-
-	INSERT INTO match (match_id, season_name, competition_name, start_date, match_day, home_club_name, out_club_name,
-					   stadium_name, referee_person_id, ball_possession_home, ball_possession_out, spectators)
-	VALUES (1, 'NL', 'Eredivisie', '2022-02-01', '2022-02-05', 'Ajax', 'Feyenoord', 'Johan Cruijff Arena', 1, 40, 60,
-			70000);
-
-	INSERT INTO player_as_reserve_in_match (player_person_id, match_id)
-	VALUES (2, 1);
-
-	INSERT INTO position (player_person_id, match_id, position_type)
-	VALUES (3, 1, 'Attacker');
-
-	EXEC tsqlt.applytrigger 'YELLOW_CARD', 'TRG_PERSON_IS_PLAYER_OR_COACH_YELLOW';
-
 	-- Expect:
 	EXEC tsqlt.expectexception @expectedmessage = 'Referee can not have a yellow card!';
 
@@ -113,36 +88,6 @@ GO
 CREATE OR ALTER PROCEDURE [Test_Trg_Person_Is_Player_Or_Coach_Yellow].[Test_Trg_Person_Is_Player_Or_Coach_Yellow_Player_Available_No_Exception]
 AS
 BEGIN
-	-- Assemble:
-	EXEC tsqlt.faketable 'PERSON';
-	EXEC tsqlt.faketable 'REFEREE';
-	EXEC tsqlt.faketable 'MATCH';
-	EXEC tsqlt.faketable 'PLAYER_AS_RESERVE_IN_MATCH';
-	EXEC tsqlt.faketable 'POSITION';
-	EXEC tsqlt.faketable 'YELLOW_CARD';
-
-	INSERT INTO person([Person_Id], [Country_Name], [First_Name], [Last_Name], [Middle_Name], [Birth_Date])
-	VALUES (1, 'Nederland', 'Oktay', 'Soyturk', NULL, '1999-05-24'),
-		   (2, 'Nederland', 'Yatko', 'Krutyos', NULL, '1999-05-31'),
-		   (3, 'Portugal', 'Cristiano', 'Ronaldo', NULL, '1999-05-11'),
-		   (4, 'Argentina', 'Lionel', 'Messi', NULL, '1999-02-15')
-
-	INSERT INTO referee([Person_Id])
-	VALUES (1);
-
-	INSERT INTO match (match_id, season_name, competition_name, start_date, match_day, home_club_name, out_club_name,
-					   stadium_name, referee_person_id, ball_possession_home, ball_possession_out, spectators)
-	VALUES (1, 'NL', 'Eredivisie', '2022-02-01', '2022-02-05', 'Ajax', 'Feyenoord', 'Johan Cruijff Arena', 1, 40, 60,
-			70000);
-
-	INSERT INTO player_as_reserve_in_match (player_person_id, match_id)
-	VALUES (2, 1);
-
-	INSERT INTO position (player_person_id, match_id, position_type)
-	VALUES (3, 1, 'Attacker');
-
-	EXEC tsqlt.applytrigger 'YELLOW_CARD', 'TRG_PERSON_IS_PLAYER_OR_COACH_YELLOW';
-
 	-- Expect:
 	EXEC tsqlt.ExpectNoException;
 
@@ -164,7 +109,7 @@ GO
 
 EXEC tsqlt.newtestclass 'test_TRG_PERSON_IS_PLAYER_OR_COACH_RED';
 
-CREATE OR ALTER PROCEDURE [Test_Trg_Person_Is_Player_Or_Coach_Red].[Test_Trg_Person_Is_Player_Or_Coach_Red_Player_Not_Available_Exception]
+CREATE OR ALTER PROCEDURE Test_Trg_Person_Is_Player_Or_Coach_Red.SETUP
 AS
 BEGIN
 	-- Assemble:
@@ -196,7 +141,12 @@ BEGIN
 	VALUES (3, 1, 'Attacker');
 
 	EXEC tsqlt.applytrigger 'RED_CARD', 'TRG_PERSON_IS_PLAYER_OR_COACH_RED';
+END
+GO
 
+CREATE OR ALTER PROCEDURE [Test_Trg_Person_Is_Player_Or_Coach_Red].[Test_Trg_Person_Is_Player_Or_Coach_Red_Player_Not_Available_Exception]
+AS
+BEGIN
 	-- Expect:
 	EXEC tsqlt.expectexception @expectedmessage = 'Player/coach is not available in match!';
 
@@ -219,36 +169,6 @@ GO
 CREATE OR ALTER PROCEDURE [Test_Trg_Person_Is_Player_Or_Coach_Red].[Test_Trg_Person_Is_Player_Or_Coach_Red_Player_Is_Referee_Exception]
 AS
 BEGIN
-	-- Assemble:
-	EXEC tsqlt.faketable 'PERSON';
-	EXEC tsqlt.faketable 'REFEREE';
-	EXEC tsqlt.faketable 'MATCH';
-	EXEC tsqlt.faketable 'PLAYER_AS_RESERVE_IN_MATCH';
-	EXEC tsqlt.faketable 'POSITION';
-	EXEC tsqlt.faketable 'RED_CARD';
-
-	INSERT INTO person([Person_Id], [Country_Name], [First_Name], [Last_Name], [Middle_Name], [Birth_Date])
-	VALUES (1, 'Nederland', 'Oktay', 'Soyturk', NULL, '1999-05-24'),
-		   (2, 'Nederland', 'Yatko', 'Krutyos', NULL, '1999-05-31'),
-		   (3, 'Portugal', 'Cristiano', 'Ronaldo', NULL, '1999-05-11'),
-		   (4, 'Argentina', 'Lionel', 'Messi', NULL, '1999-02-15')
-
-	INSERT INTO referee([Person_Id])
-	VALUES (1);
-
-	INSERT INTO match (match_id, season_name, competition_name, start_date, match_day, home_club_name, out_club_name,
-					   stadium_name, referee_person_id, ball_possession_home, ball_possession_out, spectators)
-	VALUES (1, 'NL', 'Eredivisie', '2022-02-01', '2022-02-05', 'Ajax', 'Feyenoord', 'Johan Cruijff Arena', 1, 40, 60,
-			70000);
-
-	INSERT INTO player_as_reserve_in_match (player_person_id, match_id)
-	VALUES (2, 1);
-
-	INSERT INTO position (player_person_id, match_id, position_type)
-	VALUES (3, 1, 'Attacker');
-
-	EXEC tsqlt.applytrigger 'RED_CARD', 'TRG_PERSON_IS_PLAYER_OR_COACH_RED';
-
 	-- Expect:
 	EXEC tsqlt.expectexception @expectedmessage = 'Referee can not have a red card!';
 
@@ -271,36 +191,6 @@ GO
 CREATE OR ALTER PROCEDURE [Test_Trg_Person_Is_Player_Or_Coach_Red].[Test_Trg_Person_Is_Player_Or_Coach_Red_Player_Available_No_Exception]
 AS
 BEGIN
-	-- Assemble:
-	EXEC tsqlt.faketable 'PERSON';
-	EXEC tsqlt.faketable 'REFEREE';
-	EXEC tsqlt.faketable 'MATCH';
-	EXEC tsqlt.faketable 'PLAYER_AS_RESERVE_IN_MATCH';
-	EXEC tsqlt.faketable 'POSITION';
-	EXEC tsqlt.faketable 'RED_CARD';
-
-	INSERT INTO person([Person_Id], [Country_Name], [First_Name], [Last_Name], [Middle_Name], [Birth_Date])
-	VALUES (1, 'Nederland', 'Oktay', 'Soyturk', NULL, '1999-05-24'),
-		   (2, 'Nederland', 'Yatko', 'Krutyos', NULL, '1999-05-31'),
-		   (3, 'Portugal', 'Cristiano', 'Ronaldo', NULL, '1999-05-11'),
-		   (4, 'Argentina', 'Lionel', 'Messi', NULL, '1999-02-15')
-
-	INSERT INTO referee([Person_Id])
-	VALUES (1);
-
-	INSERT INTO match (match_id, season_name, competition_name, start_date, match_day, home_club_name, out_club_name,
-					   stadium_name, referee_person_id, ball_possession_home, ball_possession_out, spectators)
-	VALUES (1, 'NL', 'Eredivisie', '2022-02-01', '2022-02-05', 'Ajax', 'Feyenoord', 'Johan Cruijff Arena', 1, 40, 60,
-			70000);
-
-	INSERT INTO player_as_reserve_in_match (player_person_id, match_id)
-	VALUES (2, 1);
-
-	INSERT INTO position (player_person_id, match_id, position_type)
-	VALUES (3, 1, 'Attacker');
-
-	EXEC tsqlt.applytrigger 'RED_CARD', 'TRG_PERSON_IS_PLAYER_OR_COACH_RED';
-
 	-- Expect:
 	EXEC tsqlt.ExpectNoException;
 
