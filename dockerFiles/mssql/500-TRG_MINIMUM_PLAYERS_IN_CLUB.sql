f60c6f20c1e301fa42c767ac8c2a27fe394e0e6b
Junjie Juan <jj.juan@student.han.nl>
2022-05-18 09:59:58 +0200

    changed trigger and unittests

--- dockerFiles/mssql/500-TRG_MINIMUM_PLAYERS_IN_CLUB.sql
+++ dockerFiles/mssql/500-TRG_MINIMUM_PLAYERS_IN_CLUB.sql
@@ -3,15 +3,18 @@ CREATE OR ALTER TRIGGER TRG_MINIMUM_PLAYERS_IN_CLUB
 	FOR INSERT, UPDATE
 	AS
 BEGIN
+	DECLARE @minimalPlayer int
+
+	set @minimalPlayer = 11
 	if @@rowcount > 0
 		BEGIN TRY
-
-			IF EXISTS(
-				SELECT COUNT(*)
+		    IF EXISTS(
+		    	SELECT COUNT(*)
 				FROM PLAYER P
+				inner join inserted I on P.PERSON_ID = I.PERSON_ID
 				GROUP BY P.CLUB_NAME
-				HAVING COUNT(*) < 11)
-				THROW 50000, 'There must be at least 11 players in a club', 1
+		        HAVING COUNT(*) < @minimalPlayer)
+				THROW 50001, 'There must be at least 11 players in a club', 1
 		END TRY
 		BEGIN CATCH
 			THROW;
