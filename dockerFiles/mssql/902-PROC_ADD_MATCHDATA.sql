33bc908f9d25fae3da8f7e64ceaf012c4d631ef8
Oktay Soytürk <o.soyturk@student.han.nl>
2022-05-18 11:01:38 +0200

    Added procedures to add match data

---
+++ dockerFiles/mssql/902-PROC_ADD_MATCHDATA.sql
@@ -0,0 +1,146 @@
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_RED_CARD(
+	@match_id g_identity,
+	@time minute_in_match,
+	@person_id person_id
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO red_card (match_id, time, person_id)
+		VALUES (@match_id, @time, @person_id);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
+
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_YELLOW_CARD(
+	@match_id g_identity,
+	@time minute_in_match,
+	@person_id person_id
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO yellow_card (match_id, time, person_id)
+		VALUES (@match_id, @time, @person_id);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
+
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_PASS(
+	@match_id g_identity,
+	@time minute_in_match,
+	@person_id person_id,
+	@success boolean
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO pass (match_id, time, person_id, succes)
+		VALUES (@match_id, @time, @person_id, @success);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
+
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_GOAL(
+	@match_id g_identity,
+	@time minute_in_match,
+	@person_id person_id
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO goal (match_id, time, person_id)
+		VALUES (@match_id, @time, @person_id);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
+
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_SHOT(
+	@match_id g_identity,
+	@time minute_in_match,
+	@person_id person_id,
+	@on_goal boolean
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO shot (match_id, time, person_id, on_goal)
+		VALUES (@match_id, @time, @person_id, @on_goal);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
+
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_FOUL(
+	@match_id g_identity,
+	@time minute_in_match,
+	@person_id person_id
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO foul (match_id, time, person_id)
+		VALUES (@match_id, @time, @person_id);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
+
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_CORNER(
+	@match_id g_identity,
+	@time minute_in_match,
+	@person_id person_id
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO corner (match_id, time, person_id)
+		VALUES (@match_id, @time, @person_id);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
+
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_CORNER(
+	@match_id g_identity,
+	@time minute_in_match,
+	@person_id person_id
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO corner (match_id, time, person_id)
+		VALUES (@match_id, @time, @person_id);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
+
+CREATE OR ALTER PROCEDURE PROC_ADD_MATCHDATA_SUBSTITUTE(
+	@match_id g_identity,
+	@time minute_in_match,
+	@in_person_id person_id,
+	@out_person_id person_id
+)
+AS
+BEGIN
+	BEGIN TRY
+		INSERT INTO substitute (match_id, time, in_person_id, out_person_id)
+		VALUES (@match_id, @time, @in_person_id, @out_person_id);
+	END TRY
+	BEGIN CATCH
+		THROW
+	END CATCH
+END
