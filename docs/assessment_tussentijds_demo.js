3edf24f9675f321e1cd070f537580cef7fc666a1
pokvful <tim.meuwsen@outlook.com>
2022-06-14 01:36:04 +0200

    removed unnecessary files

--- docs/assessment_tussentijds_demo.js
+++
@@ -1,8 +0,0 @@
-// Er is genoeg overgenomen
-db.POSITION.find().count()
-
-// Ook de goede data is overgenomen
-db.POSITION.find().limit(3)
-
-// Ook de views worden overgenomen
-db.VW_GET_PLAYROUND_DATA.find().count()
